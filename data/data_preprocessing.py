b6ecb3c613ac6416c144a979f02c6f119c9435ff
cc233 <cc33@zju.edu.cn>
2017-11-20 11:02:13 +0000

    data process finished

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -2,7 +2,25 @@ from PIL import Image
 import numpy as np
 import os
 import os.path
-rootdir='/home/cc/data/'
+import tensorflow as tf
+def crop(lr_in,hr_in):
+    with tf.device('/cpu:0'):
+        scale=2
+        height=108
+        width=108
+        overlap=12
+        hr=tf.placeholder(tf.float32,shape=[None,1024,1024,3])
+        lr=tf.placeholder(tf.float32,shape=[None,512,512,3])
+        print 'hello'
+        hr_crop = tf.extract_image_patches(hr, [1, height, width, 1], [1, height - 2 * overlap, width - 2 * overlap, 1], [1, 1, 1, 1], padding='VALID')
+        hr_reshape=tf.reshape(hr_crop, [tf.shape(hr_crop)[0] * tf.shape(hr_crop)[1] * tf.shape(hr_crop)[2], height, width, 3])
+        print 'hi'
+        lr_crop = tf.extract_image_patches(lr, [1, height/scale, width/scale, 1], [1, height/scale - 2 * overlap/scale, width/scale - 2 * overlap/scale, 1], [1, 1, 1, 1], padding='VALID')
+        lr_reshape=tf.reshape(lr_crop, [tf.shape(lr_crop)[0] * tf.shape(lr_crop)[1] * tf.shape(lr_crop)[2], height/scale, width/scale, 3])
+        sess = tf.Session()
+        return sess.run([lr_reshape,hr_reshape],{lr:lr_in,hr:hr_in})
+
+rootdir='/home/chenchen/data/dataset_scene'
 lr_path=os.path.join(rootdir,'small')
 hr_path=os.path.join(rootdir,'big')
 num=0
@@ -15,54 +33,61 @@ for parent,dirnames,filenames in os.walk(lr_path):
         num+=1
     print num
 #hr=np.zeros(num*8,1000,1000,3)
-lr=np.zeros(num*8,500,500,3)
-hr=np.zeros(num*8,1000,1000,3)
+lr=np.zeros([num*8,512,512,3],dtype=np.float32)
+hr=np.zeros([num*8,1024,1024,3],dtype=np.float32)
 #read_data
 for parent,dirnames,filenames in os.walk(lr_path):
-for dirname in dirnames:
-    image_name=os.path.join(parent,dirname)
+    filenames.sort()
+    for filename in filenames:
+        image_name=os.path.join(parent,filename)
         im=Image.open(image_name)
         im_flip=im.transpose(Image.FLIP_LEFT_RIGHT)
         data=np.array(im)
         data_flip=np.array(im_flip)
         lr[idx,:,:,:]=data
         idx+=1
-        print idx
+        #print idx
         for i in xrange(3):
             data=np.rot90(data)
             lr[idx,:,:,:]=data
             idx+=1
-            print idx
+            #print idx
         lr[idx,:,:,:]=data_flip
         idx+=1
-        print idx
+        #print idx
         for i in xrange(3):
             data_flip=np.rot90(data_flip)
             lr[idx,:,:,:]=data_flip
             idx+=1
-            print idx
+            #print idx
+print idx
+idx=0
 for parent,dirnames,filenames in os.walk(hr_path):
-for dirname in dirnames:
-    image_name=os.path.join(parent,dirname)
+    filenames.sort()
+    for filename in filenames:
+        image_name=os.path.join(parent,filename)
         im=Image.open(image_name)
         im_flip=im.transpose(Image.FLIP_LEFT_RIGHT)
         data=np.array(im)
         data_flip=np.array(im_flip)
         hr[idx,:,:,:]=data
         idx+=1
-        print idx
+        #print idx
         for i in xrange(3):
             data=np.rot90(data)
             hr[idx,:,:,:]=data
             idx+=1
-            print idx
+            #print idx
         hr[idx,:,:,:]=data_flip
         idx+=1
-        print idx
+        #print idx
         for i in xrange(3):
             data_flip=np.rot90(data_flip)
             hr[idx,:,:,:]=data_flip
             idx+=1
-            print idx
-np.save(rootdir,lr)
-np.save(rootdir,hr)
+            #print idx
+print idx
+#crop data
+lr_crop,hr_crop=crop(lr,hr)
+np.save(os.path.join(rootdir,'lr'),lr)
+np.save(os.path.join(rootdir,'hr'),hr)
--- utils/data.py
+++ utils/data.py
@@ -2,11 +2,12 @@ import scipy.sparse as sp
 import numpy as np
 import os.path
 from PIL import Image
+import tensorflow as tf
 class Dataset(object):
 
     def __init__(self, path):
         pass
-        self.input_croped,self.target_croped,self.num_train=read_data(path)
+        self.input_croped,self.target_croped,self.num_train=self.read_data(path)
         self.epoch=0
         self.index_in_epoch=0
     def next_batch(self,batch_size):
@@ -24,22 +25,12 @@ class Dataset(object):
         return self.input_croped[start:end],self.target_croped[start:end]
     def read_data(self,path):
         pass
-        lr_path=os.path.join(path,'lr.mat')
-        hr_path=os.path.join(path,'hr.mat')
-        lr=loadmat(lr_path)
-        hr=loadmat(hr_path)
+        lr_path=os.path.join(path,'lr.npy')
+        hr_path=os.path.join(path,'hr.npy')
+        print lr_path
+        lr=np.load(lr_path)
+        hr=np.load(hr_path)
         print('read data complete.')
-        lr_crop,hr_crop=crop(lr,hr)
-        return lr_crop,hr_crop
-    def crop(lr_in,hr_in)
-        scale=2
-        height=108
-        width=108
-        overlap=12
-        hr_crop = tf.extract_image_patches(hr, [1, height, width, 1], [1, height - 2 * overlap, width - 2 * overlap, 1], [1, 1, 1, 1], padding='VALID')
-        hr_reshape=tf.reshape(hr_crop, [tf.shape(hr_crop)[0] * tf.shape(hr_crop)[1] * tf.shape(hr_crop)[2], height, width, 3])
-        lr_crop = tf.extract_image_patches(lr, [1, height/scale, width/scale, 1], [1, height/scale - 2 * overlap/scale, width/scale - 2 * overlap/scale, 1], [1, 1, 1, 1], padding='VALID')
-        lr_reshape=tf.reshape(lr_crop, [tf.shape(lr_crop)[0] * tf.shape(lr_crop)[1] * tf.shape(lr_crop)[2], height/scale, width/scale, 3])
-        sess = tf.Session()
-        return sess.run([lr_reshape,hr_reshape],{lr:lr_in,hr:hr_in})
-
+        return lr,hr,lr.shape[0]
+    def get_data(self):
+        return self.input_croped,self.target_croped
